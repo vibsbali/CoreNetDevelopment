fced137beccafa178e6c57203757764c7acf5956
Vaibhav Bali <vbali@internode.on.net>
2016-05-02 14:42:44 +1000

    Added code for Greeter that extracts value from Configuration object. The IOC is also setup

--- src/CoreNetDevelopment/Services/Greeter.cs
+++ src/CoreNetDevelopment/Services/Greeter.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace CoreNetDevelopment.Services
 {
     public class Greeter : IGreeter
     {
+        public IConfiguration Configuration { get; private set; }
+        public Greeter(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public string GetGreeting()
         {
-            return "Hello from Greeter";
+            return Configuration["greeting"];
         }
     }
 }
--- src/CoreNetDevelopment/Startup.cs
+++ src/CoreNetDevelopment/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using CoreNetDevelopment.Services;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.Configuration;
@@ -22,17 +23,18 @@ namespace CoreNetDevelopment
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IGreeter greeter)
         {
             app.UseIISPlatformHandler();
 
             app.Run(async (context) =>
             {
-                var greeting = Configuration["greeting"];
+                var greeting = greeter.GetGreeting();
                 await context.Response.WriteAsync(greeting);
             });
         }
