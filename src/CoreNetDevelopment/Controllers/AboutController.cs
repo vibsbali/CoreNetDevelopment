0fe6557801188454fa473f271e77012b9af054db
Vaibhav Bali <vbali@internode.on.net>
2016-05-03 11:29:34 +1000

    Added about controller

---
+++ src/CoreNetDevelopment/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace CoreNetDevelopment.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Phone()
+        {
+            return Content("+1-555-555-555");
+        }
+
+        public IActionResult Country()
+        {
+            return Content("USA");
+        }
+    }
+}
