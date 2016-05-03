d457bfce7159beaad176ba5a7cb81bc9bb99b4d6
Vaibhav Bali <vbali@internode.on.net>
2016-05-03 12:17:49 +1000

    Added Restaurants model and created a home.cshtml file

--- src/CoreNetDevelopment/Controllers/Home.cs
+++ src/CoreNetDevelopment/Controllers/Home.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreNetDevelopment.Models;
 using Microsoft.AspNet.Mvc;
 
 namespace CoreNetDevelopment.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public object Index()
         {
-            return Content("Hello from Index Controller");
+            var restaurant = new Restaurant {Id = 1, Name = "Sabatino's"};
+            return View();
         }
     }
 }
