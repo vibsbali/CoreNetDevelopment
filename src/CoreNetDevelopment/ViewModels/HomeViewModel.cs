af36f3f60e6474b4aa05d605f2a308d42bf1f33c
Vaibhav Bali <vbali@internode.on.net>
2016-05-03 12:55:51 +1000

    Added a HomeViewModel

---
+++ src/CoreNetDevelopment/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CoreNetDevelopment.Models;
+
+namespace CoreNetDevelopment.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string CurrentGreeting { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+    }
+}
