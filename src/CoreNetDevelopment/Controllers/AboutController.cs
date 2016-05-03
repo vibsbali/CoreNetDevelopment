cc8d5abe329262ebb267f58d3a61d710ba03ba23
Vaibhav Bali <vbali@internode.on.net>
2016-05-03 11:40:43 +1000

    Configured Attribute based routing

--- src/CoreNetDevelopment/Controllers/AboutController.cs
+++ src/CoreNetDevelopment/Controllers/AboutController.cs
@@ -2,13 +2,17 @@
 
 namespace CoreNetDevelopment.Controllers
 {
+    [Route("company/[controller]")]
     public class AboutController : Controller
     {
+        //Will act as default and also respond to phone 
+        [Route("")][Route("phone")]
         public IActionResult Phone()
         {
             return Content("+1-555-555-555");
         }
 
+        [Route("[action]")]
         public IActionResult Country()
         {
             return Content("USA");
