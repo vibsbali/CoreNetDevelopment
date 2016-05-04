30fe23c6bb0337b336643c7e66f99bc46a3cb8cb
Vaibhav Bali <vibsbali@gmail.com>
2016-05-04 12:44:28 +1000

    Added a new ViewModel

---
+++ src/CoreNetDevelopment/ViewModels/RestaurantEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CoreNetDevelopment.Models;
+
+namespace CoreNetDevelopment.ViewModels
+{
+    public class RestaurantEditViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public CuisineType CuisineType { get; set; }
+    }
+}
