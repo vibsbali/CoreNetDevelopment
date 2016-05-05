f6ee849fff990a66e45a7e8814c5e7a56bc24fe1
Vaibhav Bali <vibsbali@gmail.com>
2016-05-05 13:40:31 +1000

    Completed Razor by adding a component

---
+++ src/CoreNetDevelopment/ViewComponents/Greeting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CoreNetDevelopment.Services.Greeting;
+using Microsoft.AspNet.Mvc;
+
+namespace CoreNetDevelopment.ViewComponents
+{
+    public class Greeting : ViewComponent
+    {
+        private readonly IGreeter greeter;
+
+        public Greeting(IGreeter greeter)
+        {
+            this.greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = greeter.GetGreeting();
+            return View("default", model);
+        }
+    }
+}
